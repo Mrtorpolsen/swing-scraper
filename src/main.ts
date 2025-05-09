43c61770646a7d9215b5488fbab53085fb9df12d
Torp <mrtorpolsen@gmail.com>
2025-05-09 11:46:37 +0200

    feat/ add vinci scraper

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 // For more information, see https://crawlee.dev/
-import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
+import { CheerioCrawler, ProxyConfiguration } from "crawlee";
 
-import { router } from './routes.js';
+import { router } from "./routes.js";
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = ["https://www.vinci-play.com/en/playground-equipment"];
 
 const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+  // Comment this option to scrape the full website.
+  maxRequestsPerCrawl: 5,
 });
 
 await crawler.run(startUrls);
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,52 @@
-import { createCheerioRouter } from 'crawlee';
+import { createCheerioRouter, RequestQueue } from "crawlee";
+
+interface ProductData {
+  [key: string]: string;
+}
 
 export const router = createCheerioRouter();
+const base_url = ["https://www.vinci-play.com"];
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
-    });
+router.addDefaultHandler(async ({ $, addRequests, log }) => {
+  log.info(`enqueueing new URLs`);
+  $(".cat-container li a").each((_, element) => {
+    const $element = $(element);
+    const img_src = $element.find("img").attr("src");
+    const href = $element.attr("href");
+    if (!href) {
+      log.info("no href found - skipping...");
+      return;
+    }
+
+    const url = base_url + href;
+    addRequests([
+      {
+        url,
+        label: "product",
+        userData: {
+          img_src,
+        },
+      },
+    ]);
+  });
 });
 
-router.addHandler('detail', async ({ request, $, log, pushData }) => {
-    const title = $('title').text();
-    log.info(`${title}`, { url: request.loadedUrl });
+router.addHandler("product", async ({ request, $, log, pushData }) => {
+  const title = $(".product-title").first().text();
+  const product_data: ProductData[] = [];
 
-    await pushData({
-        url: request.loadedUrl,
-        title,
-    });
+  $(".product-table tr").each((_, element) => {
+    const data_name = $(element).find(".align-left").text().trim();
+    const data_value = $(element).find(".align-right").text().trim();
+
+    product_data.push({ [data_name]: data_value });
+  });
+  log.info(`${title}`, { url: request.loadedUrl });
+
+  await pushData({
+    url: request.loadedUrl,
+    title,
+    img_src: request.userData.img_src,
+    product_data,
+  });
 });
