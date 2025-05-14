cad7a667b7f78cc6fd1e42b5e2fcd8f5a7517494
Torp <mrtorpolsen@gmail.com>
2025-05-14 13:40:39 +0200

    add/ got title and id

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,26 @@ import { CheerioCrawler, ProxyConfiguration } from "crawlee";
 import { exportToExcel } from "./utils/exportToExcel.js";
 import extractSubdomain from "./utils/extractSubdomain.js";
 
-import { router, all_products } from "./routes.js";
+/* import { router, all_products } from "./routes/vinci.js";
+const startUrls = ["https://www.vinci-play.com/en/playground-equipment"]; */
 
-const startUrls = ["https://www.vinci-play.com/en/playground-equipment"];
+import { router, all_products } from "./routes/kompagn.js";
+const startUrls = [
+  "https://www.kompan.com/da/dk/produkter?view_as=List&page=250",
+];
 
 const crawler = new CheerioCrawler({
   // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
   requestHandler: router,
   // Comment this option to scrape the full website.
-  maxRequestsPerCrawl: 1000,
+  maxRequestsPerCrawl: 5,
 });
 
 await crawler.run(startUrls);
 
-exportToExcel(
+/* exportToExcel(
   all_products,
   "./storage/datasets/excel/" +
     new Date().toISOString().replace(/[^a-zA-Z0-9]/g, ""),
   extractSubdomain(startUrls[0])
-);
+); */
--- src/routes/vinci.ts
+++ src/routes/vinci.ts
@@ -1,5 +1,5 @@
-import { createCheerioRouter, RequestQueue } from "crawlee";
-import { ProductData, Product } from "./interfaces/vinci.js";
+import { createCheerioRouter } from "crawlee";
+import { ProductData, Product } from "../interfaces/vinci.js";
 
 export const router = createCheerioRouter();
 export const all_products: Product[] = [];
@@ -32,8 +32,14 @@ router.addDefaultHandler(async ({ $, addRequests, log }) => {
 router.addHandler("product", async ({ request, $, log, pushData }) => {
   const title = $(".product-title").first().text();
   const product_data: ProductData[] = [];
+  const product_table = $(".product-table");
 
-  $(".product-table tr").each((_, element) => {
+  if (product_table.length === 0) {
+    log.warning(`No product table found for ${request.loadedUrl}`);
+    return;
+  }
+
+  product_table.find("tr").each((_, element) => {
     const data_name = $(element).find(".align-left").text().trim();
     const data_value = $(element).find(".align-right").text().trim();
 
@@ -41,16 +47,13 @@ router.addHandler("product", async ({ request, $, log, pushData }) => {
   });
   log.info(`${title}`, { url: request.loadedUrl });
 
-  all_products.push({
+  const current_product = {
     title,
     url: request.loadedUrl,
     img_src: request.userData.img_src,
     product_data,
-  });
-  await pushData({
-    title,
-    url: request.loadedUrl,
-    img_src: request.userData.img_src,
-    product_data,
-  });
+  };
+
+  all_products.push(current_product);
+  await pushData(current_product);
 });
